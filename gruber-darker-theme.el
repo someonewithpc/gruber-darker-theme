e65ca33c9adf225a83f7971d95abca55136b347f
Hugo Sales <hugo@hsal.es>
2021-11-11 11:18:18 +0000

    Tone down yellow

--- gruber-darker-theme.el
+++ gruber-darker-theme.el
@@ -54,7 +54,7 @@
       (gruber-darker-red       "#f43841")
       (gruber-darker-red+1     "#ff4f58")
       (gruber-darker-green     "#73c936")
-      (gruber-darker-yellow    "#ffdd33")
+      (gruber-darker-yellow    "#ddcc44")
       (gruber-darker-brown     "#cc8c3c")
       (gruber-darker-quartz    "#95a99f")
       (gruber-darker-niagara-2 "#303540")
